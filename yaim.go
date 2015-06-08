7c99cd0fb4e14d14b447ac9af5e2e4a9ab8ec9e0
Wayne You <wei.you@outlook.com>
2015-06-08 15:02:26 +0800

    add js/css handler

--- yaim.go
+++ yaim.go
@@ -6,36 +6,91 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"io/ioutil"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
-var indexTpl = template.Must(template.ParseFiles("templates/index.html"))
+const (
+	TemplatePath string = "templates"
+	PublicPath string = "public"
+)
 
-func serveHttp(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", 404)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	indexTpl.Execute(w, r.Host)
-}
-
-func serveWS(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("serve ws")
-}
+var (
+	addr = flag.String("addr", ":8080", "http service address")
+	templates *template.Template
+)
 
 func main() {
-	fmt.Println("yaim")
 	flag.Parse()
+
+	templates = parseTemplates()
+
 	http.HandleFunc("/", serveHttp)
-	http.HandleFunc("/ws", serveWS)
+	http.HandleFunc("/ws", serveWs)
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
+
+func serveHttp(w http.ResponseWriter, r *http.Request) {
+	var (
+		contentType string
+		isTpl bool
+	)
+
+	path := r.URL.Path[1:]
+
+	if strings.HasSuffix(path, ".css") {
+		contentType = "text/css"
+	} else if strings.HasSuffix(path, ".js") {
+		contentType = "text/javascript"
+	} else {
+		contentType = "text/html"
+		isTpl = true
+	}
+
+	w.Header().Set("Content-Type", contentType + "; charset=utf-8")
+	if isTpl {
+		if path == "" {
+			path = "index"
+		}
+		template := templates.Lookup(path + ".html")
+		if template == nil {
+			w.WriteHeader(404)
+			return
+		}
+		template.Execute(w, nil)
+	} else {
+		data, err := ioutil.ReadFile(PublicPath + "/" + string(path))
+		if err != nil {
+			w.WriteHeader(404)
+			return
+		}
+		w.Write(data)
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("serve ws")
+}
+
+func parseTemplates() *template.Template {
+	result := template.New("templates")
+
+	templateFolder, _ := os.Open(TemplatePath)
+	defer templateFolder.Close()
+
+	templatePathsRaw, _ := templateFolder.Readdir(-1)
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths, TemplatePath + "/" + pathInfo.Name())
+		}
+	}
+
+	result.ParseFiles(*templatePaths...)
+	return result
+}
+
